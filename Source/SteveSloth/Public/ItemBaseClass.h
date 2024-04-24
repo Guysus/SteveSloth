9de40c75fa8bc03d7f354d21255d07f9b9491b97
Guysus <137240103+Guysus@users.noreply.github.com>
2024-04-24 11:28:38 -0700

    Update ItemBaseClass.h

--- Source/SteveSloth/Public/ItemBaseClass.h
+++ Source/SteveSloth/Public/ItemBaseClass.h
@@ -6,6 +6,25 @@
 #include "GameFramework/Actor.h"
 #include "ItemBaseClass.generated.h"
 
+//All of the different ammo types in an enum
+enum AmmoType
+{
+	Pebble,
+	Firecracker,
+	WaterBalloon,
+	PoisonSac,
+	ChristmasBeetle,
+	BigRock
+};
+
+//Different types of health the player can pick up
+enum HealthType
+{
+	RedFlower,
+	BlueFlower,
+	GreenFlower
+};
+
 UCLASS()
 class STEVESLOTH_API AItemBaseClass : public AActor
 {
@@ -23,4 +42,21 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammo")
+	//Amount of ammo picked up
+	int AmmoAmount;
+	//Chance of what kind of ammo is going to drop
+	float ChanceOfAmmo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Currency")
+	//Amount of grubs picked up
+	int Grubs;
+	//Amount of eucalyptus picked up
+	int Eucalyptus;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health Type")
+	//Counts amount of health to add
+	int FlowerBud;
+	//Chance in percentage of what kind of health drop
+	float ChanceOfHealth;
 };
