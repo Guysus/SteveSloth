06134ee6de08b8bf8458695f3fa281f4be1a980f
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2024-04-25 14:22:11 -0400

    Edited MyPlayer.cpp and added IMC for inputs

--- Source/SteveSloth/Private/MyPlayer.cpp
+++ Source/SteveSloth/Private/MyPlayer.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 * Copyright: SteveSloth
- * Name: Tammy Boisvert
+ * Name: Tammy Boisvert edited by Jeff
  * Script: MyPlayer.cpp
  * Date: April 23. 2024
  * Description: This is the Player Base Class Script
@@ -10,13 +10,10 @@
 
 #include "MyPlayer.h"
 
-// Sets default values
 AMyPlayer::AMyPlayer()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	//Setting Default Values:
+	
 	MaxHealth = 0;
 	CurrentHealth = 0;
 	GrubsCollected = 0;
@@ -24,24 +21,20 @@ AMyPlayer::AMyPlayer()
 	LevelsCompleted = 0;
 	BossesKilled = 0;
 	LeavesFound = 0;
-
 }
 
-// Called when the game starts or when spawned
 void AMyPlayer::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AMyPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AMyPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/SteveSloth/Public/MyPlayer.h
+++ Source/SteveSloth/Public/MyPlayer.h
@@ -1,6 +1,6 @@
 /****************************************************************************************
  * Copyright: SteveSloth
- * Name: Tammy Boisvert
+ * Name: Tammy Boisvert edited by Jeff
  * Script: MyPlayer.h
  * Date: April 23. 2024
  * Description: This is the Player Base Class Script
@@ -14,7 +14,6 @@
 #include "GameFramework/Character.h"
 #include "MyPlayer.generated.h"
 
-
 enum class AmmoTypes
 {
 	Pebble,
@@ -25,28 +24,20 @@ enum class AmmoTypes
 	BigRock
 };
 
-enum class Abilities
-{
-
-};
-
 UCLASS()
 class STEVESLOTH_API AMyPlayer : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AMyPlayer();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override; 
-
+	
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float MaxHealth;
 
@@ -56,18 +47,12 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int CurrentSlingshotAmmo;
 
-
-	// Count Levels completed
+	
 	int LevelsCompleted;
-	//Count Bosses Killed
 	int BossesKilled;
-	//Count Leaves Found
 	int LeavesFound;
-	//Count Grubs Collected
     int GrubsCollected;
 
-
-	// Called to bind functionality to input
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
