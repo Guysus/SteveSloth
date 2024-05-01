a0411b6b8eddbca5d7ac9739793d871ca545cb68
MysticAmazon <129459054+MysticAmazon@users.noreply.github.com>
2024-04-30 22:02:14 -0400

    Created Script
    planned out on paper how to go about it

---
+++ Source/SteveSloth/Private/MyEnemyAIChasingState.cpp
@@ -0,0 +1,20 @@
+/****************************************************************************************
+ * Copyright: SteveSloth
+ * Name: Tammy Boisvert 
+ * Script: MyEnemyAIChasingState.cpp
+ * Date: April 30. 2024
+ * Description: This is the Enemy State for chasing the player when they are in range
+ * TODO:
+ * Known Bugs:
+ ****************************************************************************************/
+
+
+#include "MyEnemyAIChasingState.h"
+
+MyEnemyAIChasingState::MyEnemyAIChasingState()
+{
+}
+
+MyEnemyAIChasingState::~MyEnemyAIChasingState()
+{
+}
---
+++ Source/SteveSloth/Public/MyEnemyAIChasingState.h
@@ -0,0 +1,21 @@
+/****************************************************************************************
+ * Copyright: SteveSloth
+ * Name: Tammy Boisvert
+ * Script: MyEnemyAIChasingState.h
+ * Date: April 30. 2024
+ * Description: This is the Enemy State for chasing the player when they are in range
+ * TODO:
+ * Known Bugs:
+ ****************************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyEnemyBaseClass.h"
+
+class STEVESLOTH_API MyEnemyAIChasingState
+{
+public:
+	MyEnemyAIChasingState();
+	~MyEnemyAIChasingState();
+};
