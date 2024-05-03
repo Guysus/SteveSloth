58a951cde895ddc4c6d1ca171dcb202f6cae5846
Kenster833 <129459004+Kenster833@users.noreply.github.com>
2024-05-03 18:56:50 -0400

    Started C++ class

---
+++ Source/SteveSloth/Public/MyEnemyAttackingState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyEnemyBaseState.h"
+#include "MyEnemyAttackingState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEVESLOTH_API UMyEnemyAttackingState : public UMyEnemyBaseState
+{
+	GENERATED_BODY()
+	
+};
