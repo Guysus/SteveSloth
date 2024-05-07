ca13035a2cc0260c0d58bf4dfba3965098f80991
Kenster833 <129459004+Kenster833@users.noreply.github.com>
2024-05-07 18:52:12 -0400

    Start of Taking and implementing feedback

--- Source/SteveSloth/Private/MyEnemyAttackingState.cpp
+++ Source/SteveSloth/Private/MyEnemyAttackingState.cpp
@@ -10,12 +10,11 @@
 
 
 #include "MyEnemyAttackingState.h"
-#include "MyEnemyBaseClass.h"
-#include "Kismet/GameplayStatics.h"
+#include "MySteveSingleton.h"
 
 void UMyEnemyAttackingState::EnterState()
 {
-
+	Player = USteveSingleton::GetSteve()->GetPlayerCharacter();
 }
 
 void UMyEnemyAttackingState::ExitState()
@@ -25,45 +24,6 @@ void UMyEnemyAttackingState::ExitState()
 
 void UMyEnemyAttackingState::UpdateState(float deltaTime)
 {
-	if (IsPlayerInRange())
-	{
-		AttackPlayer();
-	}
+	
 }
 
-bool UMyEnemyAttackingState::IsPlayerInRange() const
-{
-	AMyEnemyBaseClass* Owner = Cast<AMyEnemyBaseClass>(GetOuter());
-	if (!Owner)
-	{
-		UE_LOG(LogTemp, Error, TEXT("MyEnemyAttackingState::IsPlayerInRange - Owner is not valid"));
-		return false;
-	}
-
-	ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
-	if (!PlayerCharacter)
-	{
-		UE_LOG(LogTemp, Error, TEXT("MyEnemyAttackingState::IsPlayerInRange - Player character not found"));
-		return false;
-	}
-
-	float Distance = FVector::Distance(Owner->GetActorLocation(), PlayerCharacter->GetActorLocation());
-	return Distance <= Owner->GetAttackRange();
-}
-
-void UMyEnemyAttackingState::AttackPlayer()
-{
-	AMyEnemyBaseClass* Owner = Cast<AMyEnemyBaseClass>(GetOuter());
-	if (!Owner)
-	{
-		UE_LOG(LogTemp, Error, TEXT("MyEnemyAttackingState::AttackPlayer - Owner is not valid"));
-		return;
-	}
-
-	ACharacter* PlayerCharacter = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
-	if (!PlayerCharacter)
-	{
-		UE_LOG(LogTemp, Error, TEXT("MyEnemyAttackingState::AttackPlayer - Player character not found"));
-		return;
-	}
-}
--- Source/SteveSloth/Public/MyEnemyAttackingState.h
+++ Source/SteveSloth/Public/MyEnemyAttackingState.h
@@ -12,12 +12,11 @@
 
 #include "CoreMinimal.h"
 #include "MyEnemyBaseState.h"
+#include "Kismet/GameplayStatics.h"
+#include "MySteveSingleton.h"
 #include "MyEnemyAttackingState.generated.h"
 
-/**
- * 
- */
-UCLASS()
+UCLASS(Blueprintable)
 class STEVESLOTH_API UMyEnemyAttackingState : public UMyEnemyBaseState
 {
 	GENERATED_BODY()
@@ -27,7 +26,6 @@ public:
 	virtual void ExitState() override;
 	virtual void UpdateState(float deltaTime) override;
 
-private:
-	bool IsPlayerInRange() const;
-	void AttackPlayer();
+protected:
+	ACharacter* Player;
 };
