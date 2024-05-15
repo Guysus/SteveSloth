6aa22e8f686d9e9a757b0687a6dbf558b6a5e357
Saretzky <eysaretzky@yahoo.ca>
2024-05-14 20:28:18 -0400

    made data script, removed unused enemy

---
+++ Source/SteveSloth/Public/MyEnemyData.h
@@ -0,0 +1,15 @@
+/****************************************************************************************
+* Copyright: SteveSloth
+ * Name: Elad Saretzky
+ * Script: MyEnemyData.h
+ * Date: May 14 2024
+ * Description: this has all the variables used in the enemy scripts
+ * TODO:
+ * Known Bugs:
+ ****************************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
