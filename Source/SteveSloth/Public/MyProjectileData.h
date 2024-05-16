5914eb5faaef8109615e016323617dceb210d050
Saretzky <eysaretzky@yahoo.ca>
2024-05-16 18:26:56 -0400

    commented out old script, filled out data table

--- Source/SteveSloth/Public/MyProjectileData.h
+++ Source/SteveSloth/Public/MyProjectileData.h
@@ -14,9 +14,51 @@
 #include "Engine/DataTable.h"
 #include "MyProjectileData.generated.h"
 
+UENUM(BlueprintType)
+enum class EProjectileType
+{
+	Physical UMETA(DisplayName = "Physical"),
+	Fire UMETA(DisplayName = "Fire"),
+	Water UMETA(DisplayName = "Water"),
+	Poison UMETA(DisplayName = "Poison"),
+	Ice UMETA(DisplayName = "Ice")
+};
+
 USTRUCT(BlueprintType)
 struct STEVESLOTH_API FMyProjectileData : public FTableRowBase
 {
 	GENERATED_BODY()
 	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TEnumAsByte<EProjectileType> ProjectileType;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float Damage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float ProjectileSpeed;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float ProjectileRange;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float ProjectileGravity;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float InitialLaunchAngle;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float AreaOfEffectDamage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float AreaOfEffectRadius;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float DamageOverTime;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float DamageOverTimeDuration;
 };
