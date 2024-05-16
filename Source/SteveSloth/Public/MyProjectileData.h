b2ca879f75b8731c0eb0572cab25821ecd5dd6e5
Saretzky <eysaretzky@yahoo.ca>
2024-05-16 16:55:27 -0400

    scripts made, files reorganized

---
+++ Source/SteveSloth/Public/MyProjectileData.h
@@ -0,0 +1,22 @@
+/****************************************************************************************
+* Copyright: SteveSloth
+ * Name: Elad Saretzky
+ * Script: MyProjectileData.h
+ * Date: May 16, 2024
+ * Description: data table that holds all the projectile variables
+ * TODO:
+ * Known Bugs:
+ ****************************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "MyProjectileData.generated.h"
+
+USTRUCT(BlueprintType)
+struct STEVESLOTH_API FMyProjectileData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+};
