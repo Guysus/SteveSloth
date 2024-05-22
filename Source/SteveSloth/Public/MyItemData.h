7037a7be5664ce352bfe08b4e85c964fb3539bdf
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2024-05-21 21:23:16 -0400

    works but i dont like the u

---
+++ Source/SteveSloth/Public/MyItemData.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+
+
+#include "MyItemData.generated.h"
+
+USTRUCT(BlueprintType)
+struct STEVESLOTH_API FUMyItemData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+};
