3a9e25a814f12f30a5950846a326fcd1667f5f06
Saretzky <eysaretzky@yahoo.ca>
2024-05-22 18:07:51 -0400

    added hud script, commiting to switch branches and review other branch

---
+++ Source/SteveSloth/Public/MyPlayerHeadsUpDisplay.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyPlayerHeadsUpDisplay.generated.h"
+
+UCLASS()
+class STEVESLOTH_API UMyPlayerHeadsUpDisplay : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
