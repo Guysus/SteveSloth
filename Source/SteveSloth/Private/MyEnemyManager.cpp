f4bd6be9b9d4279340e27934731bec5002f3743d
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2024-05-30 21:56:26 -0400

    yup

--- Source/SteveSloth/Private/MyEnemyManager.cpp
+++ Source/SteveSloth/Private/MyEnemyManager.cpp
@@ -18,7 +18,24 @@ AMyEnemyManager::AMyEnemyManager()
 void AMyEnemyManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	ULevel* currentLevel = GetWorld()->GetCurrentLevel();
+	FString levelName = currentLevel->GetOutermost()->GetName();
+
+	static ConstructorHelpers::FObjectFinder<UClass> Koala(TEXT("Blueprint'/Game/Blueprints/Enemies/EnemyProjectiles/BP_KoalaStickAmmo.BP_KoalaStickAmmo_C'"));
+	UClass* theKoala = Koala.Object;
+	FActorSpawnParameters spawnParams;
+
+	if (levelName == "LevelOne")
+	{
+		for (int i = 0; i < SpawnPoints.Num(); i++)
+		{
+			FVector SpawnLocation = SpawnPoints[i]->GetTransform().GetLocation();
+			FRotator SpawnRotation = SpawnPoints[i]->GetTransform().GetRotation().Rotator();
+
+			AActor* tryThis = GetWorld()->SpawnActor<AActor>(theKoala, SpawnLocation, SpawnRotation, spawnParams);
+		}
+	}
 }
 
 void AMyEnemyManager::Tick(float DeltaTime)
