a794515cb375484afb7545add20095c5a1fab66b
Saretzky <eysaretzky@yahoo.ca>
2024-06-07 13:03:12 -0400

    Made respawn point script

---
+++ Source/SteveSloth/Private/MyRespawnPoint.cpp
@@ -0,0 +1,36 @@
+/****************************************************************************************
+ * Copyright: SteveSloth
+ * Name: Elad Saretzky
+ * Script: MyRespawnPoint.cpp
+ * Date: June 7, 2024
+ * Description: Updates the respawn point for the player
+ * TODO:
+ * Known Bugs:
+ ****************************************************************************************/
+
+#include "MyRespawnPoint.h"
+
+// Sets default values
+AMyRespawnPoint::AMyRespawnPoint()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyRespawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMyRespawnPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMyRespawnPoint::OnHitboxOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
